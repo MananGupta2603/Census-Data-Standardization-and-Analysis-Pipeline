4e335053bfb4642868628e20e9cd5cce8d70cc64
MananGupta2603 <mananngupta.official@gmail.com>
2024-06-07 10:14:30 +0530

    update files

--- Task 7/task7.15.py
+++ Task 7/task7.15.py
@@ -10,7 +10,7 @@ password=""
 )
 mycursor=mydb.cursor(buffered=True)
 #-------------------------------------
-mycursor.execute("SELECT State_UT,Type_of_latrine_facility_Pit_latrine_Households,Type_of_latrine_facility_Other_latrine_Households,TypeoflatrinefacilityNightsoildisposedintoopendrainHouseholds As Type_of_latrine_facility_Night_soil_disposed_into_open_drain_Households,T_LatrineFac_FP_ConOtherSys_HH As Type_of_latrine_facility_Flush_pour_flush_latrine_connected_to_other_system_Households FROM census_db.census Group By state_ut")
+mycursor.execute("SELECT State_UT,sum(Type_of_latrine_facility_Pit_latrine_Households) As Type_of_latrine_facility_Pit_latrine_Households,sum(Type_of_latrine_facility_Other_latrine_Households) AS Type_of_latrine_facility_Other_latrine_Households,sum(TypeoflatrinefacilityNightsoildisposedintoopendrainHouseholds) As Type_of_latrine_facility_Night_soil_disposed_into_open_drain_Households,sum(T_LatrineFac_FP_ConOtherSys_HH) As Type_of_latrine_facility_Flush_pour_flush_latrine_connected_to_other_system_Households FROM census_db.census Group By state_ut")
 out=mycursor.fetchall()
 
 df=pd.DataFrame(out,columns=[i[0]for i in mycursor.description])
--- Task 7/task7.18.py
+++ Task 7/task7.18.py
@@ -11,7 +11,7 @@ password=""
 )
 mycursor=mydb.cursor(buffered=True)
 #-------------------------------------
-#--> how this code work ex--> state= ladakh, sum=41467 ,count=2(number of state) percentage= (41467*100)/2=2073350
+#--> how this code work ex--> state= ladakh, sum=41467 ,count=2(number of state) percentage= (41467*100)/2=2073350  
 
 mycursor.execute("SELECT State_UT, (sum(Married_couples_1_Households+Married_couples_2_Households+Married_couples_3_Households+Married_couples_3_or_more_Households+Married_couples_4_Households+Married_couples_5__Households+Married_couples_None_Households)*100)/sum(Household_size_1_person_Households+Household_size_2_persons_Households+Household_size_1_to_2_persons+Household_size_3_persons_Households+Household_size_3_to_5_persons_Households+Household_size_4_persons_Households+Household_size_5_persons_Households+Household_size_6_8_persons_Households+Household_size_9_persons_and_above_Households) As percentage FROM census_db.census Group By state_ut")
 out=mycursor.fetchall()
